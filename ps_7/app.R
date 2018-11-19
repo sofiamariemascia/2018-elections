faef6ee3d2b7f42b729ea831f258d4a05edfea2a
Sofia Marie Mascia <smascia@college.harvard.edu>
2018-11-18 19:26:14 -0600

    axis error fixed

--- ps_7/app.R
+++ ps_7/app.R
@@ -12,7 +12,7 @@ library(base)
 
 # Define UI for application.
 
-app_data <- read_rds("ps_7.rds")
+california <- read_rds("ps_7.rds")
 
 ui <- fluidPage(
   
@@ -23,12 +23,12 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       h4("Plot Parameters"),
-      selectInput("y", "Y-axis:",
+      selectInput("x", "X-axis:",
                   choices = c("trump16", "clinton16", "romney12", "obama12", "demhouse16", "rephouse16", 
                               "white_pct", "black_pct", "hispanic_pct", "nonwhite_pct", "foreignborn_pct",
                               "median_hh_inc", "lesshs_pct","lesscollege_pct"),
                   selected = "trump16"),
-      selectInput("x", "Color By:",
+      selectInput("y", "Color By:",
                   c("county"),
                   selected = "real_rep_adv"),
       hr(),

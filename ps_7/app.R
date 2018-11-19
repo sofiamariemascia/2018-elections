e63eeb13a24264b8702295a0e513954a8871e929
Sofia Marie Mascia <smascia@college.harvard.edu>
2018-11-18 19:24:25 -0600

    app configuration fixed?

--- ps_7/app.R
+++ ps_7/app.R
@@ -1,50 +1,67 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
+library(tidyverse)
+library(stringr)
+library(ggplot2)
+library(kableExtra)
+library(lubridate)
+library(knitr)
+library(readr)
+library(dplyr)
+library(haven)
+library(base)
 
-# Define UI for application that draws a histogram
+# Define UI for application.
+
+app_data <- read_rds("ps_7.rds")
+
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  
+  # Creating a Title
+  titlePanel("California Midterm 2018 Election Context"),
+  
+  # Formatting the layout of the page 
+  sidebarLayout(
+    sidebarPanel(
+      h4("Plot Parameters"),
+      selectInput("y", "Y-axis:",
+                  choices = c("trump16", "clinton16", "romney12", "obama12", "demhouse16", "rephouse16", 
+                              "white_pct", "black_pct", "hispanic_pct", "nonwhite_pct", "foreignborn_pct",
+                              "median_hh_inc", "lesshs_pct","lesscollege_pct"),
+                  selected = "trump16"),
+      selectInput("x", "Color By:",
+                  c("county"),
+                  selected = "real_rep_adv"),
+      hr(),
+      helpText("Data from Upshot NYT")
+    ),
+    
+    
+    mainPanel(
+      plotOutput("barplot")
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$barplot <- renderPlot({
+    
+    
+    ##Read in the results data from UPSHOT
+    california %>% 
+      ggplot(aes_string(x = input$x, y = input$y, fill = input$y)) +
+      geom_bar(position = "dodge") +
+      theme_linedraw(base_size = 13) +
+      ggtitle(paste(input$x, "and", input$y, "Demographics of the Sample")) + 
+      labs(x = input$x, y = "Count")
+    
+    
+  })
 }
 
-# Run the application 
+
+# Run the application below. 
+
 shinyApp(ui = ui, server = server)
 
